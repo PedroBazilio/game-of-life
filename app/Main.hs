090e31124637eca7ef6dcc6f60600ef2477868c7
PedroBazilio <pedrobazilio@id.uff.br>
2023-10-01 22:22:49 -0300

    first data types and matrix generator

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
-module Main (main) where
+-- module Main (main) where
+import System.Random
+import Control.Monad (replicateM)
 
-import Lib
+-- import Lib
+
+-- main :: IO ()
+-- main = someFunc
+-- 1 is alive, 2 is Dead and 3 is Zombie
+data State = Dead | Alive | Zombie deriving (Eq, Show)
+data Position = Position Integer Integer
+type Generation = Position -> State
+type Grid = [[Int]]
+
+
+
+-- Generating numbers from 1 to 3
+randomValue :: IO Int
+randomValue = randomRIO (1, 3)
+
+-- Creating a matrix from the giving sizes
+generateRandomMatrix :: Int -> Int -> IO Grid
+generateRandomMatrix numRows numCols = do
+    let generateRow = replicateM numCols randomValue
+    replicateM numRows generateRow
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Enter the number of rows: "
+    numRows <- readLn
+    putStrLn "Enter the number of columns: "
+    numCols <- readLn
+
+    randomMatrix <- generateRandomMatrix numRows numCols
+
+    putStrLn "Random Matrix:"
+    mapM_ print randomMatrix
