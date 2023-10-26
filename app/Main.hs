f9d3d6693be970df225b500946e041af9c41d18c
PedroBazilio <pedrobazilio@id.uff.br>
2023-10-26 19:32:35 -0300

    ajustando comentarios e criando função de verificar vizinhos

--- app/Main.hs
+++ app/Main.hs
@@ -8,17 +8,22 @@ import Control.Monad (replicateM)
 -- main = someFunc
 -- 1 is alive, 2 is Dead and 3 is Zombie
 data State = Dead | Alive | Zombie deriving (Eq, Show)
-data Position = Position Integer Integer
-type Generation = Position -> State
+data Coord = Coord Integer Integer deriving (Eq, Show)
+type Generation = Coord -> State
 type Grid = [[Int]]
 
 
 
--- Generating numbers from 1 to 3
+-- Gerando numeros de 1 a 3
 randomValue :: IO Int
 randomValue = randomRIO (1, 3)
 
--- Creating a matrix from the giving sizes
+-- Função para verificar se é vizinho
+adjacent :: Coord -> Coord -> Bool
+adjacent p q | p == q = False
+adjacent (Coord x1 y1) (Coord x2 y2) = abs (x1 - x2) <= 1 && abs (y1 - y2) <= 1
+
+-- Criando matrizes com os tamanhos dados
 generateRandomMatrix :: Int -> Int -> IO Grid
 generateRandomMatrix numRows numCols = do
     let generateRow = replicateM numCols randomValue
