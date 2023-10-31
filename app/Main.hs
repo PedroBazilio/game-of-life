6ea3ce184e37ea06dfb426c09ed076958d005cb4
Keren Duarte <barbaraguarino.edu@gmail.com>
2023-10-31 18:58:39 -0300

    verificação de entrada de dados.

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,14 @@
 -- module Main (main) where
+import Lib
+import GHC.Generics (prec)
 import System.Random
 import Control.Monad (replicateM)
-import GHC.Generics (prec)
 
--- import Lib
-
--- main :: IO ()
--- main = someFunc
 -- 1 is alive, 2 is Dead and 3 is Zombie
 data State = Dead | Alive | Zombie deriving (Eq, Show)
 data Coord = Coord Integer Integer deriving (Eq, Show)
+
 type Generation = Coord -> State
-type Grid = [[Int]]
-type Line = [Int]
 
 -- Verificações de estados
 isAlive :: State -> Bool
@@ -45,54 +41,38 @@ neighbors (Coord x y) =
   , Coord (x-1) y
   ]
 
-
 -- Criando matrizes com os tamanhos dados
 generateRandomMatrix :: Int -> Int -> IO Grid
 generateRandomMatrix numRows numCols = do
     let generateRow = replicateM numCols randomValue
     replicateM numRows generateRow
 
--- Adicionar linha na matrix
-addLine :: Grid -> Line -> IO Grid
-addLine m line = return (m ++ [line])
-
--- Ler linha de numeros do terminal
-getLinha :: IO Line
-getLinha = do
-  line <- getLine
-  let nums = map read (words line) :: [Int]
-  return nums
-
--- Subtração
-calSubtr :: Int -> IO Int
-calSubtr n = return (n - 1)
-
--- Criar matriz
-creatMatrix :: Int -> Grid -> IO Grid
-creatMatrix numRows matriz = do
-  if numRows <= 0
-    then return matriz
-    else do
-      line <- getLinha
-      matriz <- addLine matriz line
-      numRows <- calSubtr numRows
-      creatMatrix numRows matriz
 
 main :: IO ()
 main = do
-    putStrLn "Enter the number of rows: "
-    numRows <- readLn
-    putStrLn "Enter the number of columns: "
-    numCols <- readLn
 
-    randomMatrix <- generateRandomMatrix numRows numCols
+  putStrLn " "
+  putStrLn "Numero de linhas: "
+  numRows <- readLn
 
-    putStrLn "Entre com as linhas da matriz:"
-    putStrLn "OBS: utlize espaçamento entre os numeros e aperte enter apos digitar cada linha."
-    m <- creatMatrix numRows []
+  putStrLn "Numero de colunas: "
+  numCols <- readLn
 
-    putStrLn "Random Matrix:"
-    mapM_ print randomMatrix
+  putStrLn " "
 
-    putStrLn "Leitura:"
-    mapM_ print m
+  putStrLn "OBS: Insera cada linha de forma verifical\ne os numeros separados por espaço."
+  putStrLn "Ex:\n1 2 1\n2 3 1\n1 1 2\n"
+  putStrLn "Insira os dados da matriz: "
+  m <- creatMatrix numRows numCols [] 1
+  putStrLn " "
+  putStrLn "Matriz de Entrada:"
+  mapM_ print m
+
+  putStrLn " "
+
+  randomMatrix <- generateRandomMatrix numRows numCols
+
+  putStrLn "Random Matrix:"
+  mapM_ print randomMatrix
+
+  putStrLn " "
